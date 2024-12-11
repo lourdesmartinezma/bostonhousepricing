3518be26bd304331fab71a1b64a645cc3434c2e7
lourdesmartinezma <lourdesmartinezma>
2024-12-11 16:46:11 +0000

    Create a new branch and commit changes to an external Git repo

--- Git Operations.py
+++ Git Operations.py
@@ -1,3 +1,4 @@
 # Databricks notebook source
 print('Pushing changes to the remote Git repo')
-print('Pull changes from the remote Git repository')
+print('Pull changes from the remote Git repo')
+print('Create a new branch and commit changes to an external Git repo')
